c085bd3d3c8d133976a5d11fd3baa2261b3729eb
rita48690651@gmail.com <rita48690651@gmail.com>
2021-08-23 19:47:45 +0800

    加入專案檔案。

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,31 @@
+﻿using BFTFCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BFTFCalculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View(new Calculator());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Calculator c)
+        {
+            if (int i = 1;i< c.period;i++)
+            {
+                c.average = (((1 + c.rate) * c.period) * c.rate) / (((1 + c.rate) * c.period) - 1);//計算每月應付本息金額之平均攤還率
+                c.due = c.money * c.average;//計算每月應付款(每月應還本金金額+每月應付利息金額)
+                c.interest = c.remain * c.rate;//計算每月應付利息
+                c.PayPrinciple = c.due - c.interest;//計算每月應還本金
+            }
+            return View(c);
+        }
+    }
+}
---
+++ Models/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BFTFCalculator.Models
+{
+    public class Calculator
+    {
+        public int money { get; set; }//(給使用者輸入的)借貸金額
+
+        public  int rate { get; set; }//(給使用者輸入的)年利率
+
+        public int period { get; set; }//I給使用者輸入的)貸款期數
+
+        public int result { get; set; }
+
+        public int due { get; set; }//當期應付款(當期應償還本金+當期應償還利息)
+
+        public int PayPrinciple { get; set; }//當期應償還本金
+
+        public int interest  { get; set; }//當期應償還利息
+
+        public int remain { get; set; }//剩餘本金
+
+        public int average { get; set; }//每月應付本息金額之平均攤還率
+    }
+}
